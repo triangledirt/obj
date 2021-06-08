e8999443a11b29a212deab111fab1e31a6ea1cb6
Matthew Temple <matthew@triangledirt.com>
2021-06-08 15:22:01 -0400

    added LEARN_FREQ and alib_learn() to alib.c

--- alib.c
+++ alib.c
@@ -3,11 +3,13 @@
 #include "aobj.h"
 
 #define OBJECT_CACHE 1024
+#define LEARN_FREQ 32
 
 static char initialized = 0;
 static aobj_t objs[OBJECT_CACHE];
 
 static void alib_init();
+static void alib_learn();
 
 void alib_init() {
   if (!initialized) {
@@ -20,10 +22,15 @@ void alib_init() {
   }
 }
 
+void alib_learn() {}
+
 void alib_notice(aobj_t obj) {
   alib_init();
   long idx = random() % OBJECT_CACHE;
   objs[idx] = obj;
+  if (0 == (random() % LEARN_FREQ)) {
+    alib_learn();
+  }
 }
 
 abit_t alib_classify(aobj_t obj) {
