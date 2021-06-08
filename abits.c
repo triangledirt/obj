c76ee760fb7eed526eed9e00e73be35bb7668f2c
Matthew Temple <matthew@triangledirt.com>
2021-06-08 17:21:35 -0400

    moved away from makefile to local clean, make

--- abits.c
+++ abits.c
@@ -10,5 +10,6 @@ void abits_learn(aobj_t objs[], long objs_size) {
   aobj_t obj;
   for (idx = 0; idx < objs_size; idx++) {
     obj = objs[idx];
+    if (obj > 0) {};
   }
 }
---
+++ make
@@ -0,0 +1,2 @@
+gcc -ansi -pedantic -Wall -Werror -O3 -D_DEFAULT_SOURCE -c *.c
+ar rcs alib.a *.o
