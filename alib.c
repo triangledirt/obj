2d1147963d51a08c085c1c146b5d7c17186a15d6
Matthew Temple <matthew@triangledirt.com>
2021-06-08 20:48:50 -0400

    added support for multiple object types

--- alib.c
+++ alib.c
@@ -4,41 +4,57 @@
 #include "aobj.h"
 
 #define OBJECT_CACHE 1024
-#define LEARN_FREQ 32
+#define TYPES_MAX 8
 
-static char initialized = 0;
-static aobj_t objs[OBJECT_CACHE];
+static char initd = 0;
+static aobj_t objs[TYPES_MAX][OBJECT_CACHE];
+static long types = 1;
 
 static void alib_init();
 static void alib_learn();
+static void alib_uptypes(long seentype);
 
 void alib_init() {
   long idx;
-  if (!initialized) {
-    for (idx = 0; idx < OBJECT_CACHE; idx++) {
-      objs[idx] = random();
+  long type;
+  if (!initd) {
+    for (type = 0; type < TYPES_MAX; type++) {
+      for (idx = 0; idx < OBJECT_CACHE; idx++) {
+        objs[type][idx] = random();
+      }
     }
   } else {
-    initialized = 1;
+    initd = 1;
   }
 }
 
 void alib_learn() {
-  abits_learn(objs, OBJECT_CACHE);
+  long type;
+  for (type = 0; type < TYPES_MAX; type++) {
+    abits_learn(objs[type], OBJECT_CACHE);
+  }
 }
 
-void alib_notice(aobj_t obj) {
+void alib_notice(aobj_t obj, long type) {
   long idx;
   alib_init();
+  alib_uptypes(type);
   idx = random() % OBJECT_CACHE;
-  objs[idx] = obj;
-  if (0 == (random() % LEARN_FREQ)) {
+  objs[type][idx] = obj;
+  if (0 == (random() % (OBJECT_CACHE / 32))) {
     alib_learn();
   }
 }
 
-abit_t alib_classify(aobj_t obj) {
+abit_t alib_classify(aobj_t obj, long type) {
   alib_init();
+  alib_uptypes(type);
   return abits_classify(obj);
   return 0;
+}
+
+void alib_uptypes(long seentype) {
+  if ((seentype + 1) > types) {
+    types = seentype;
+  }
 }
