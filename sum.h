aa50354e4c6cdcd51d4e29949362e54f3608e123
Matthew Temple <matthew@triangledirt.com>
2021-12-26 13:05:24 -0500

    made function header match definition

--- sum.h
+++ sum.h
@@ -4,7 +4,7 @@
 #include "bit.h"
 #include "obj.h"
 
-void sum_learn(case_obj_t objs[], long objssz, long type);
+void sum_learn(case_obj_t obj[], long objsz, long type);
 case_bit_t sum_classify(case_obj_t obj, long type);
 
 #endif
