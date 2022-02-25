2d64ec67bbe675073059b13ff0ae15665e779074
Matthew Temple <matthew@triangledirt.com>
2022-02-25 17:37:28 +0000

    re-ordered obj declarations

--- obj.h
+++ obj.h
@@ -12,9 +12,6 @@ void case_obj_setfromstr(case_obj_t *obj, char *str);
 #define case_obj_setattr(obj, indx, val) if (val) { *obj |= ((long) 1 << (indx)); } else { *obj &= ~((long) 1 << (indx)); }
 #define case_obj_getattr(obj, indx) ((obj >> (indx)) & (long) 1)
 
-void case_obj_mutate(case_obj_t *obj);
-void case_obj_rotate(case_obj_t *obj, long inc);
-
 #define case_obj_setclass(obj, val) if (val) { *obj |= ((long) 1); } else { *obj &= ~((long) 1); }
 #define case_obj_getclass(obj) (obj & (long) 1)
 
@@ -22,13 +19,16 @@ double case_obj_cmpequal(case_obj_t obj1, case_obj_t obj2);
 double case_obj_cmpsmash(case_obj_t obj1, case_obj_t obj2);
 double case_obj_cmptypes(case_obj_t obj1, case_obj_t obj2);
 
+case_bool_t case_obj_hastype(case_obj_t obj, case_obj_t type);
+
+void case_obj_mutate(case_obj_t *obj);
+void case_obj_rotate(case_obj_t *obj, long inc);
+
 void case_obj_print(case_obj_t obj);
 
-case_bool_t case_obj_hastype(case_obj_t obj, case_obj_t type);
+double case_obj_getobliv(case_obj_t obj1, case_obj_t obj2);
 
 void case_obj_setnum(case_obj_t *obj, long startbit, long bits, long num);
 long case_obj_getnum(case_obj_t obj, long startbit, long bits);
-
-double case_obj_getobliv(case_obj_t obj1, case_obj_t obj2);
 
 #endif
