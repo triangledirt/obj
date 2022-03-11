30874a605b655e5214a0f54ca978dd2601cb38ce
Matthew Temple <matthew@triangledirt.com>
2022-03-11 14:27:27 -0500

    added pack_f packfunc() param to testfile()

--- test.c
+++ test.c
@@ -10,10 +10,12 @@
 static long total;
 static long correct;
 
+typedef case_obj_t (*pack_f)(char *, long, long);
+
 static void reset();
 static void summarize();
-static void testfile(char *filename, long classindx, long type);
-static void testline(char *line, long classindx, long type);
+static void testfile(char *filename, long classindx, long type, pack_f packfunc);
+static void testline(char *line, long classindx, long type, pack_f packfunc);
 static void testrand(long type);
 
 void reset()
@@ -29,25 +31,25 @@ void summarize()
 #endif
 }
 
-void testfile(char *filename, long classindx, long type)
+void testfile(char *filename, long classindx, long type, pack_f packfunc)
 {
   FILE *file;
   char line[CASE_CSVOBJ];
   reset();
   file = fopen(filename, "r");
   while (fgets(line, CASE_CSVOBJ, file))
-    testline(line, classindx, type);
+    testline(line, classindx, type, packfunc);
   fclose(file);
   summarize();
 }
 
-void testline(char *line, long classindx, long type)
+void testline(char *line, long classindx, long type, pack_f packfunc)
 {
   case_obj_t obj;
   char c;
   case_bit_t guessclass;
   case_bit_t actualclass;
-  obj = case_packfirst(line, classindx, type);
+  obj = packfunc(line, classindx, type);
   case_observe(obj, type);
   guessclass = case_classify(obj, type);
   actualclass = case_obj_getclass(obj);
@@ -96,8 +98,8 @@ void testrand(long type)
 int main(int argc, char **argv)
 {
   if (0) testrand(RANDOM);
-  if (0) testfile("data/diabetes.csv", 0, DIABETES);
-  if (0) testfile("data/drug.csv", 0, DRUG);
-  if (0) testfile("data/forest.csv", 0, FOREST);
-  if (1) testfile("data/mushroom.csv", 0, MUSHROOM);
+  if (0) testfile("data/diabetes.csv", 0, DIABETES, case_packfirst);
+  if (0) testfile("data/drug.csv", 3, DRUG, case_packfirst);
+  if (0) testfile("data/forest.csv", 0, FOREST, case_packavg);
+  if (1) testfile("data/mushroom.csv", 0, MUSHROOM, case_packavg);
 }
