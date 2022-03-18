cf395818687ea70dbe76b86e329a2d3431f8563c
Matthew Temple <matthew@triangledirt.com>
2022-03-18 16:34:15 +0000

    removed unnecessary files

--- trace.h
+++
@@ -1,9 +0,0 @@
-#ifndef trace_h
-#define trace_h
-
-unsigned char trace_randomize();
-
-unsigned char trace_calc(unsigned char indx);
-unsigned char trace_program(unsigned char indx, unsigned char val);
-
-#endif
