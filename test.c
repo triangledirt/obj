8883b0ce216f1e2c169c488282a60b4dd72db359
Matthew Temple <matthew@triangledirt.com>
2022-03-23 17:21:02 +0000

    removed cover.csv from test

--- test.c
+++ test.c
@@ -3,16 +3,15 @@
 
 #define CENSUS 0
 #define CONNECT4 1
-#define COVER 2
-#define LETTER 3
-#define MUSHROOM 4
-#define MUSHROOM2 5
-#define NAMEGENDER 6
-#define NURSERY 7
-#define OCCUPANCY 8
-#define POKER 9
-#define SEPSIS 10
-#define SHUTTLE 11
+#define LETTER 2
+#define MUSHROOM 3
+#define MUSHROOM2 4
+#define NAMEGENDER 5
+#define NURSERY 6
+#define OCCUPANCY 7
+#define POKER 8
+#define SEPSIS 9
+#define SHUTTLE 10
 
 static void summarize(long type);
 static void testpack(char *filename, long classindx, long type, case_pack_f packfunc);
@@ -58,7 +57,6 @@ int main(int argc, char *argv[])
 {
   if (0) testpack("data/census.csv", 14, CENSUS, case_packavg);
   if (0) testpack("data/connect4.csv", 0, CONNECT4, case_packavg);
-  if (0) testpack("data/cover.csv", 0, COVER, case_packavg);
   if (0) testpack("data/letter.csv", 0, LETTER, case_packavg);
   if (1) testpack("data/mushroom.csv", 0, MUSHROOM, case_packavg);
   if (0) testpack("data/mushroom2.csv", 0, MUSHROOM2, case_packavg);
