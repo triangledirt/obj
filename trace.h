959abfb97f1f68d9dbdb8166976707acdd00986e
Matthew Temple <matthew@triangledirt.com>
2022-04-11 17:48:59 +0000

    added back in trace play files

---
+++ trace.h
@@ -0,0 +1,9 @@
+#ifndef trace_h
+#define trace_h
+
+unsigned char trace_randomize();
+
+unsigned char trace_calc(unsigned char indx);
+unsigned char trace_program(unsigned char indx, unsigned char val);
+
+#endif
