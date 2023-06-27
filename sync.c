1fe63fa7ccba19614626001d43cdbaccf29660f4
dirt <dirt@fastmail.com>
2023-06-27 18:39:59 +0300

    worked on sync

--- sync.c
+++ sync.c
@@ -6,6 +6,9 @@ static obj_t object[OBJ_TYPE][OBJ_SYNC_CACHE];
 static enum obj_syncstyle_t syncstyle[OBJ_TYPE];
 static obj_bool_t valid[OBJ_TYPE];
 static obj_bool_t once = obj_bool_false;
+static obj_t syncset[OBJ_TYPE][OBJ_SYNC_SET][OBJ_SYNC_CACHE];
+static long setcnt[OBJ_TYPE][OBJ_SYNC_SET];
+static long setnext[OBJ_TYPE][OBJ_SYNC_SET];
 
 static void init();
 static void sync(long type);
@@ -25,6 +28,8 @@ void init()
         obj_randomize(&obj);
         object[i][j] = obj;
       }
+      for (j = 0; j < OBJ_SYNC_SET; j++)
+        setcnt[i][j] = 0;
     }
     once = obj_bool_true;
   }
@@ -39,10 +44,19 @@ void obj_sync_add(obj_t obj, long type)
   valid[type] = obj_bool_false;
 }
 
-obj_bool_t obj_sync_next(obj_t *next, long set, long type)
+obj_t obj_sync_next(long set, long type)
 {
+  long next;
+  obj_t obj;
   init();
-  ;;
+  next = setnext[type][set];
+  if (next < OBJ_SYNC_SET) {
+    obj = syncset[type][set][next];
+    setnext[type][set]++;
+  } else {
+    obj_clear(&obj);
+  }
+  return obj;
 }
 
 void obj_sync_remove(obj_t obj, long type)
@@ -69,7 +83,7 @@ void obj_sync_start(long set, long type)
 {
   init();
   sync(type);
-  ;;
+  setnext[type][set] = 0;
 }
 
 void sync(long type)
--- sync.h
+++ sync.h
@@ -10,10 +10,7 @@
 
 void obj_sync_setstyle(enum obj_syncstyle_t style, long type);
 
-void obj_sync_add(obj_t obj, long type);
-void obj_sync_remove(obj_t obj, long type);
-
 void obj_sync_start(long set, long type);
-obj_bool_t obj_sync_next(obj_t *next, long set, long type);
+obj_t obj_sync_next(long set, long type);
 
 #endif
