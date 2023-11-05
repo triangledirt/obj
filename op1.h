c8081df43111099a36f6d5592e9fab7e5bae746b
dirt <dirtlove@proton.me>
2023-11-05 09:56:18 -0600

    created op1

---
+++ op1.h
@@ -0,0 +1,12 @@
+#ifndef obj_op1_h
+#define obj_op1_h
+
+#include "bit.h"
+
+typedef char obj_op1_t;
+
+obj_bit_t obj_op1_calc(obj_op1_t op1, obj_bit_t bit);
+
+void obj_op1_randomize(obj_op1_t *op1);
+
+#endif
