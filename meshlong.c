dfd3492ed711377e3ddc4aa05a3bffa43650a900
dirt <dirtlove@proton.me>
2024-03-03 14:57:53 -0600

    removed unused function

--- meshlong.c
+++ meshlong.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include "meshlong.h"
 
-char obj_meshlong_face(struct obj_meshlong_t *meshlong)
-{
-  char c = 97;
-  c += 0;
-  ;;
-  return c;
-}
-
 void obj_meshlong_print(struct obj_meshlong_t *meshlong)
 {
   printf("game %d\n", meshlong->game);
--- meshlong.h
+++ meshlong.h
@@ -8,7 +8,6 @@ struct obj_meshlong_t {
   char outaddr;
 };
 
-char obj_meshlong_face(struct obj_meshlong_t *meshlong);
 void obj_meshlong_print(struct obj_meshlong_t *meshlong);
 
 #endif
