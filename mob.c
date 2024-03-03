fdb6e1d6392d3dcffac8de02b51276a96643e5f7
dirt <dirtlove@proton.me>
2024-03-03 11:37:23 -0600

    fixed i2xy()

--- mob.c
+++ mob.c
@@ -34,8 +34,8 @@ void *i2xy(long i, long *x, long *y)
     *y = -1;
     break;
   case 1:
-    *x = -1;
-    *y = 0;
+    *x = 0;
+    *y = -1;
     break;
   case 2:
     *x = 1;
@@ -54,17 +54,19 @@ void *i2xy(long i, long *x, long *y)
     *y = 0;
     break;
   case 6:
-    *x = 1;
-    *y = -1;
+    *x = -1;
+    *y = 1;
     break;
   case 7:
-    *x = 1;
-    *y = 0;
+    *x = 0;
+    *y = 1;
     break;
   case 8:
     *x = 1;
     *y = 1;
     break;
+  default:
+    exit(12903);
   }
 }
 
