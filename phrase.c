3c1ef5e8d6c2117122e5e74446449f3bfea641a3
dirt <dirtlove@proton.me>
2024-03-04 06:57:15 -0600

    added randomize() and mutate() functions to obj::phrase

--- phrase.c
+++ phrase.c
@@ -1,9 +1,10 @@
+#include <stdlib.h>
 #include "phrase.h"
 
 enum obj_bool_t obj_phrase_addmesh(struct obj_phrase_t *phrase, struct obj_mesh_t *mesh)
 {
   enum obj_bool_t success;
-  if (phrase->meshsz < MESH_MAX) {
+  if (phrase->meshsz < OBJ_PHRASE_MESH) {
     phrase->mesh[phrase->meshsz] = *mesh;
     phrase->meshsz++;
     success = obj_bool_true;
@@ -25,9 +26,24 @@ void obj_phrase_init(struct obj_phrase_t *phrase)
   phrase->meshsz = 0;
 }
 
+void obj_phrase_mutate(struct obj_phrase_t *phrase)
+{
+  long i;
+  i = random() % phrase->meshsz;
+  obj_mesh_mutate(&phrase->mesh[i]);
+}
+
 void obj_phrase_print(struct obj_phrase_t *phrase)
 {
   long i;
   for (i = 0; i < phrase->meshsz; i++)
     obj_mesh_print(&phrase->mesh[i]);
+}
+
+void obj_phrase_randomize(struct obj_phrase_t *phrase)
+{
+  long i;
+  phrase->meshsz = random() % OBJ_PHRASE_MESH;
+  for (i = 0; i < phrase->meshsz; i++)
+    obj_mesh_init(&phrase->mesh[i]);
 }
--- phrase.h
+++ phrase.h
@@ -4,16 +4,20 @@
 #include "bool.h"
 #include "mesh.h"
 
-#define MESH_MAX 16
+#define OBJ_PHRASE_MESH 16
 
 struct obj_phrase_t {
-  struct obj_mesh_t mesh[MESH_MAX];
+  struct obj_mesh_t mesh[OBJ_PHRASE_MESH];
   long meshsz;
 };
 
 void obj_phrase_init(struct obj_phrase_t *phrase);
 enum obj_bool_t obj_phrase_addmesh(struct obj_phrase_t *phrase, struct obj_mesh_t *mesh);
 void obj_phrase_apply(struct obj_phrase_t *phrase, struct obj_mesh_t *data);
+
+void obj_phrase_randomize(struct obj_phrase_t *phrase);
+void obj_phrase_mutate(struct obj_phrase_t *phrase);
+
 void obj_phrase_print(struct obj_phrase_t *phrase);
 
 #endif
